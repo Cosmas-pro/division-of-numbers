987926af2d4b9474e65205a79583a3c568921416
Cosmas-pro <91739410+Cosmas-pro@users.noreply.github.com>
2021-10-15 11:39:46 +0300

    Add files via upload

---
+++ division of number.c
@@ -0,0 +1,13 @@
+/*Arithmetic operation division - user friendly program*/
+#include <stdio.h>
+int main()
+{
+int a, b, Division;
+printf("\n enter value to A:");
+scanf("%d",&a);
+printf("\n Enter value to B:");
+scanf("%d",&b);
+Division =a/b;
+printf("\n Division=&",Division);
+return 0;
+}
